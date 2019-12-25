deebf9bd6869fca10b5a2fa34a4ab4467a79b580
Nishan Chathuranga Wickramarathna <inbox.nishan@gmail.com>
2019-12-25 12:54:12 +0530

    more endpoints

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 import numpy as np
 import pickle
+import hashlib
+import json
+import re
 
+from bson import json_util
 from flask import Flask, request, jsonify, render_template, abort
 from flask_pymongo import PyMongo 
 from flask_cors import CORS, cross_origin
-# MONGO_URI = 'mongodb+srv://strainer_admin:strainer_admin123@strainercluster-igrpg.azure.mongodb.net/strainer?retryWrites=true&w=majority'
+
 mongo = PyMongo()
 
 app = Flask(__name__)
 
-# app.config.from_object('settings')
 app.config["MONGO_URI"] = 'mongodb+srv://strainer_admin:strainer_admin123@strainercluster-igrpg.azure.mongodb.net/strainer?retryWrites=true&w=majority'
 
 CORS(app)
@@ -30,16 +33,42 @@ def home():
 def query():
     if not request.json or not 'query' in request.json:
         abort(400)
+    queryId = int(hashlib.sha256(request.json['query'].encode('utf-8')).hexdigest(), 16) % 10**8
     query = {
+        'queryId': queryId,
         'name': request.json['screenname'],
         'query': request.json['query'],
         'done': False
     }
-    # print(request.json['screenname'])
     query_collection = mongo.db.queries
+    result = json.dumps(list(query_collection.find({'queryId' : queryId},{ "_id": 0, "queryId": 1 })), default=json_util.default)
+    if result != "[]":
+        claim = str(re.findall("\d+", result))
+        return jsonify({'message': 'That claim is already exists with Id ' + claim + ', Please use it to check results'}), 400
     query_collection.insert(query)
-    return jsonify('Query added successfully'), 201
-    # return jsonify({'query': query}), 201
+    return jsonify({'queryId': queryId,'message': 'Query added successfully'}), 201
+
+@app.route('/notdone', methods=['GET'])
+@cross_origin()
+def notdone():
+    query_collection = mongo.db.queries
+    docs_list = list(query_collection.find({'done' : False},{ "_id": 0, "queryId": 1, "query": 1 }))
+    return json.dumps(docs_list, default=json_util.default, indent=4, sort_keys=True)
+
+@app.route('/setdone/<id>', methods=['POST'])
+@cross_origin()
+def setdone(id):
+    try:
+        queryId = int(id)
+    except:
+        return jsonify({'message': 'Not a valid query Id'}), 400
+    
+    query_collection = mongo.db.queries
+    result = json.dumps(list(query_collection.find({'queryId' : queryId},{ "_id": 0, "queryId": 1 })), default=json_util.default)
+    if result == "[]":
+        return jsonify({'queryId': queryId, 'message': 'No claim is available for that query Id'}), 400
+    query_collection.update({ "queryId": queryId },{ '$set': { "done": True }})
+    return jsonify({'message': 'Query updated as done'}), 200
 
 @app.route('/predict',methods=['POST'])
 @cross_origin()
